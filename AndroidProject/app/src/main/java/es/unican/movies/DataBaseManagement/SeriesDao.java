a3b3d8445e00e437b5be8636e993bb7f1dc908b5
Victor <vgl946@alumnos.unican.es>
2025-10-15 15:41:20 +0200

    Creacion SeriesDao

---
+++ AndroidProject/app/src/main/java/es/unican/movies/DataBaseManagement/SeriesDao.java
@@ -0,0 +1,22 @@
+package es.unican.movies.DataBaseManagement;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface SeriesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void addToWishlist(SeriesDB seriesDB);
+
+    @Query("SELECT * FROM SeriesDB")
+    List<SeriesDB> getWishlist();
+
+    @Delete
+    void removeFromWishlist(SeriesDB seriesDB);
+}
